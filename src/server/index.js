f393cbe7749ced79a7d5cb4c205ad4245fd767c4
Garrett Tongue <garrettjtongue@gmail.com>
2018-02-09 14:38:10 -0800

    Initial socket stuff

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const path = require("path");
 
-app.use(express.static(__dirname +'./../../'));
-app.listen(3000, () => {
+
+app.use(express.static(path.join(__dirname, "./../../")));
+
+app.get('/', function(req, res,next) {
+  res.sendFile(path.join(__dirname, "./../../index.html"));
+});
+
+io.on('connection', function (socket) {
+  console.log("new client connecteD");
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+server.listen(3000, () => {
   console.log("Listening on Port 3000");
 });
+
